dd35a61c0d9cf7723b69132951b824fd9dee39c9
Hegifebrianto <hegifebrianto@gmail.com>
2025-02-03 22:51:14 +0700

    chore:add nextconfig domain myanimelist

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ["cdn.myanimelist.net"], // Tambahkan domain ini
+  },
 };
 
 export default nextConfig;
